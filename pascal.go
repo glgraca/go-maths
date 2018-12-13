5124be4a7776a54384457b0dfe9848f4cd951cac
glgraca <30938064+glgraca@users.noreply.github.com>
2018-12-13 05:25:09 -0200

    Add files via upload
    This programs prints the nth line of Pascal's triangle by recursively calling a function that returns a channel through which each line is fed.

---
+++ pascal.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strconv"
+)
+
+func pascal(n uint64) <-chan uint64 {
+  out:=make(chan uint64)
+  if n==0 {
+    go func() {
+      out<-1
+      close(out)
+    }()
+  } else {
+    in:=pascal(n-1)
+    go func() {
+      a:=<-in
+      out<-1
+      for b:=range in {
+        out<-a+b
+        a=b
+      }
+      out<-1
+      close(out)
+    }()
+  }
+  return out;
+}
+
+func main() {
+  n,_:=strconv.ParseUint(os.Args[1], 10, 64);
+  line:=pascal(n);
+  for n:=range line {
+    fmt.Printf("%d ", n);
+  }
+}
